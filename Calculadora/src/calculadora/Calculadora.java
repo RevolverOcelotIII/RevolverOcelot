2f2c9744656aa55a279634a047b8c4d65f41cf6a
Daniel <revolverocelot.r3v0lv3r0c3l0t@gmail.com>
2019-03-28 18:41:38 -0300

    Calculadora Insert

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author danpg
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame Interface=new JFrame();
+        Interface.setSize(500,500);
+        Interface.setTitle("CALCULADORA");
+        Interface.setVisible(true);
+    }
+    
+}
---
+++ Calculadora/src/calculadora/Calculos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author danpg
+ */
+public class Calculos {
+    public int Soma(int val1,int val2){
+        return val1+val2;
+    }
+    public double Divisao(int val1,int val2){
+        return val1/val2;
+    }
+    public int Multiplicacao(int val1, int val2){
+        return val1*val2;
+    }
+    public int Subtracao(int val1, int val2){
+        return val1-val2;
+    }
+}
